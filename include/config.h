548221ba0639246ffbcb17e2a4ad3c9f661d4835
Matic Knez <matic.knez82@gmail.com>
2024-11-07 09:38:12 +0100

    Update even odd sort end early

--- include/config.h
+++ include/config.h
@@ -8,10 +8,9 @@
 #include <pthread.h>
 #include <time.h>
 #include <stddef.h>
-#include <stdatomic.h>
 
 #define NTHREADS 3
 
 #define __PRINT__
 
-#endif /* CONFIG_H */
+#endif /* CONFIG_H */                   
--- src/vaje/even_odd_sort_block.c
+++ src/vaje/even_odd_sort_block.c
@@ -1,10 +1,10 @@
 #include "config.h"
 
-#define N 15      // Number of elements (naj bo sodo)
+#define N 15   // Number of elements (naj bo sodo)
 
 // NUmber of elements each thread should process
 const size_t chunk = N/NTHREADS;
-const bool even = (N % 2 == 0);
+const bool even_n = (N % 2 == 0);
 
 
 typedef struct {
@@ -17,21 +17,21 @@ pthread_t threads[NTHREADS];
 args_t arguments[NTHREADS];
 pthread_barrier_t barrier;
 
+int pass_cnt = 0;
 bool sorted = true;
-
 
 void *sort(void *args);
 
 void swap(int *pa, int *pb, int id);
-void process_even_chunk(int id, size_t chunk, int *pseznam, pthread_barrier_t *barrier);
-void process_odd_chunk(int id, size_t chunk, int *pseznam, pthread_barrier_t *barrier);
-void printSeznam(int*);
+void process_even_chunk(int id);
+void process_odd_chunk(int id);
+void printList(int *pseznam);
 
 int main() {
     printf("NTHREADS: %d\n", NTHREADS);
     printf("N elements: %d\n", N);
-    printf("CHUNK size: %d\n", chunk);
-
+    printf("CHUNK size: %ld\n", chunk);
+    printf("Pass type: %s\n", (chunk % 2 == 0 ? "even" : "odd"));
 
     // N elementov
     pseznam = (int*)malloc(N*sizeof(int));
@@ -42,8 +42,8 @@ int main() {
         // *(pseznam + i)
         pseznam[i] = rand() % 100;
     }
-
-    printSeznam(pseznam);
+    printf("Unordered list: ");
+    printList(pseznam);
     printf("\n");
 
     // Init barrier
@@ -63,51 +63,56 @@ int main() {
         pthread_join(threads[i], NULL);
     }
 
+    printf("Orderd list   : ");
+    printList(pseznam);
 
     return 0;
 }
 
 void *sort(void *args) {
+    args_t *parguments = (args_t*)args;
+    int id = parguments->id;
 
     for (size_t i = 0; i < N; i++)
     {
-        args_t *parguments = (args_t*)args;
-        int id = parguments->id;
-
-        // if (konec) break;
-        printf("Waiting at start... thread %d\n", id);
-        sorted = true;
-        pthread_barrier_wait(&barrier);
-
+        if (id == 0) {
+            // Pass counter
+            pass_cnt++;
+        }
+        // Do even + odd pass based of N/NTHREADS
         if (chunk % 2 == 0) {
-            process_even_chunk(id, chunk, pseznam, &barrier);
+            process_even_chunk(id);
         }
         else {
-            process_odd_chunk(id, chunk, pseznam, &barrier);
+            process_odd_chunk(id);
         }
+
+        // Wait for all threads to finish both even and odd passes
+        // before reading from variable sorted
         pthread_barrier_wait(&barrier);
-        printf("Thread %d sees sorted=0x%x before if\n", id, sorted);
+
         if (sorted) {
-            printf("Thread %d sorted = 0x%x\n", id, sorted);
-            printf("Exited thread %d\n", id);
+            //printf("Thread %d exited\n", id);
             break;
         }
-        // All threads need to check sorted 
-        // before nex titteration where sorted is changed back to true
+        // Wait for all threads to read sorted (process if)
+        // before setting sorted back to true
         pthread_barrier_wait(&barrier);
 
+        // Reset sorted to true before next itteration
+        // Only thread with id == 0
+        if (id == 0) {
+            sorted = true;  
+        }   
 
+        // Thread 0 has to set sorted = true 
+        // before any of threads start new itteration
+        // where they can set sorted to false
+        pthread_barrier_wait(&barrier);
+    }
 
-
-        // if id == 0 and urejen == true --> set konec = true
-        // ? WHYY 
-
-
-        // if (finished)
-        // {
-        //     // Should not break??? Why?? 
-        //     break;
-        // }
+    if (id == 0) {
+        printf("Number of passes: %d\n", pass_cnt);
     }
 
     return NULL;
@@ -118,89 +123,99 @@ void swap(int *pa, int *pb, int id) {
         int tmp = *pa;
         *pa = *pb;
         *pb = tmp;
-
+        
         sorted = false;
-        printf("Thread %d set sorted=false\n", id);
     }
 }
 
-void process_even_chunk(int id, size_t chunk, int *pseznam, pthread_barrier_t *barrier) {
+void process_even_chunk(int id) {
         // Even pass
         for (size_t j = id * chunk; j < (id + 1) * chunk; j+=2) 
         {
             //printf("Thread %d, even pass, index:  %d\n", id, j);
             swap(pseznam + j, pseznam + j + 1, id);
         }
-        pthread_barrier_wait(barrier);
+
+        // Wait all threads to finish even pass before starting odd pass
+        pthread_barrier_wait(&barrier);
 
 #ifdef __PRINT__
         if (id == 0) {
-            printf("Even pass: ");
-            printSeznam(pseznam);
+            printf("Even pass : ");
+            printList(pseznam);
         }
 #endif
         // Odd pass
         bool last = (id == NTHREADS - 1);
-        for (size_t j = id * chunk + 1; j < (id + 1) * chunk - (last && even); j+=2)
+        for (size_t j = id * chunk + 1; j < (id + 1) * chunk - (last && even_n); j+=2)
         {
             // if even N + last thread: (id + 1) * CHUNK -1
             // if odd N + last thread: (id + 1) * CHUNK
             //printf("Thread %d, odd pass, index:  %d\n", id, j);
             swap(pseznam + j, pseznam + j + 1, id);
         }
-        pthread_barrier_wait(barrier);
 
 #ifdef __PRINT__
         if (id == 0) {
             printf("Odd pass: ");
-            printSeznam(pseznam);
+            printList(pseznam);
             printf("===========================================\n");
         }
 #endif
-        pthread_barrier_wait(barrier); // WHY HERE???? 
 }
 
-void process_odd_chunk(int id, size_t chunk, int *pseznam, pthread_barrier_t *barrier) {
+void process_odd_chunk(int id) {
         // Even pass
         bool last = (id == NTHREADS - 1);
-        for (size_t j = id * chunk + (id % 2); j < (id + 1) * chunk - (last && !even); j+=2) 
+        // for (size_t j = id * chunk + (id % 2); j < (id + 1) * chunk - (last && !even_n); j+=2) 
+        // {
+        //     printf("Thread %d, even pass, index:  %d\n", id, j);
+        //     swap(pseznam + j, pseznam + j + 1, id);
+        // }
+        for (size_t j = id * chunk + (id % 2); j < (id + 1) * chunk- (last && !even_n); j+=2) 
         {
-            //printf("Thread %d, even pass, index:  %d\n", id, j);
+            printf("Thread %d, even pass, index:  %d\n", id, j);
             swap(pseznam + j, pseznam + j + 1, id);
         }
-        pthread_barrier_wait(barrier);
+
+        // Wait all threads to finish even pass before starting odd pass
+        pthread_barrier_wait(&barrier);
 
 #ifdef __PRINT__
         if (id == 0) {
             printf("Even pass: ");
-            printSeznam(pseznam);
+            printList(pseznam);
         }
 #endif
 
         // Odd pass
-        for (size_t j = id * chunk + (id % 2 == 0); j < (id + 1) * chunk; j+=2)
+        // for (size_t j = id * chunk + (id % 2 == 0); j < (id + 1) * chunk; j+=2)
+        // {
+        //     // if even N + last thread: (id + 1) * CHUNK -1
+        //     // if odd N + last thread: (id + 1) * CHUNK
+        //     printf("Thread %d, odd pass, index:  %d\n", id, j);
+        //     swap(pseznam + j, pseznam + j + 1, id);
+        // }
+
+        //bool last = (id == NTHREADS - 1);
+        for (size_t j = id * chunk + (id % 2 == 0); j < (id + 1) * chunk + (last && even_n); j+=2)
         {
             // if even N + last thread: (id + 1) * CHUNK -1
             // if odd N + last thread: (id + 1) * CHUNK
-            //printf("Thread %d, odd pass, index:  %d\n", id, j);
+            printf("Thread %d, odd pass, index:  %d\n", id, j);
             swap(pseznam + j, pseznam + j + 1, id);
         }
-        pthread_barrier_wait(barrier);
 
 #ifdef __PRINT__
         if (id == 0) {
             printf("Odd pass: ");
-            printSeznam(pseznam);
+            printList(pseznam);
             printf("===========================================\n");
         }
 #endif
-        pthread_barrier_wait(barrier);
-
-
-        return NULL;
 }
 
-void printSeznam(int *pseznam) {
+void printList(int *pseznam) {
     for (int i = 0; i < N; i++) {
         printf("%d ", pseznam[i]);
     }
